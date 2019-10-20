9db12f9d471254e9ae51daeda80605282a7a62ef
Taha Alami Idrissi <miala69@hotmail.fr>
2019-10-20 12:37:56 +0200

    every folder is a project if it doesnt exit create

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+
+#each website you crawl is a separate project(folder)
+#new folder = new project
+
+def create_project_dir(directory):
+    #if the folder (project)does not  already exist
+    
+    if not os.path.exists(directory):
+        print("Creating project"+directory)
+        os.makedirs(directory)
+        
+create_project_dir("talassi")
