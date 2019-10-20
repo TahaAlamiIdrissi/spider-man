0379bfbe623b02c1084e71518c40a112bd30cca8
Taha Alami Idrissi <miala69@hotmail.fr>
2019-10-20 12:39:03 +0200

    general file gonna contain shared functions

--- general.py
+++ general.py
@@ -7,7 +7,7 @@ def create_project_dir(directory):
     #if the folder (project)does not  already exist
     
     if not os.path.exists(directory):
-        print("Creating project"+directory)
+        print("Creating project "+directory)
         os.makedirs(directory)
         
 create_project_dir("talassi")
